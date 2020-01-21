5228dc698b40ea910772ed565470cf1955449a1f
Robert Gmyr <rogmyr@microsoft.com>
2020-01-21 14:06:27 -0800

    Forward transform through interface

--- infinibatch/torch/chunked_dataset.py
+++ infinibatch/torch/chunked_dataset.py
@@ -7,7 +7,7 @@ from infinibatch import common
 class ChunkedDataset(IterableDataset):
     def __init__(self, path, shuffle=False, buffer_size=1024, transform=None):
         super().__init__()
-        self.dataset = common.chunked_dataset.ChunkedDataset(path, shuffle=shuffle, buffer_size=buffer_size)
+        self.dataset = common.chunked_dataset.ChunkedDataset(path, shuffle=shuffle, buffer_size=buffer_size, transform=transform)
 
 
     def __iter__(self):
