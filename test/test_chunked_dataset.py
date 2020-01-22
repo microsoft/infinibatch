767d14b75d76beed4baef6b12f047ce62f2dc141
Robert Gmyr <rogmyr@microsoft.com>
2020-01-21 18:03:43 -0800

    Simplify test code

--- test/test_chunked_dataset.py
+++ test/test_chunked_dataset.py
@@ -56,16 +56,12 @@ class TestChunkedDataset(unittest.TestCase):
 
 
     def test_chunked_data_generator_no_shuffle(self):
-        items = []
-        for item in chunked_data_generator(self.chunk_file_paths, shuffle_chunks=False):
-            items.append(item)
+        items = list(chunked_data_generator(self.chunk_file_paths, shuffle_chunks=False))
         self.assertListEqual(items, self.flattened_test_data)
 
 
     def test_chunked_data_generator_shuffle(self):
-        items = []
-        for item in chunked_data_generator(self.chunk_file_paths, shuffle_chunks=True):
-            items.append(item)
+        items = list(chunked_data_generator(self.chunk_file_paths, shuffle_chunks=True))
         self.assertSetEqual(set(items), set(self.flattened_test_data))
 
     
@@ -92,16 +88,12 @@ class TestChunkedDataset(unittest.TestCase):
 
 
     def test_buffered_shuffle_generator(self):
-        items = []
-        for item in buffered_shuffle_generator(self.flattened_test_data.copy(), 971):
-            items.append(item)
+        items = list(buffered_shuffle_generator(self.flattened_test_data.copy(), 971))
         self.assertSetEqual(set(items), set(self.flattened_test_data))
 
 
     def test_buffered_shuffle_generator_buffer_size_one(self):
-        items = []
-        for item in buffered_shuffle_generator(self.flattened_test_data.copy(), 1):
-            items.append(item)
+        items = list(buffered_shuffle_generator(self.flattened_test_data.copy(), 1))
         self.assertSetEqual(set(items), set(self.flattened_test_data))
 
 
@@ -110,39 +102,26 @@ class TestChunkedDataset(unittest.TestCase):
 
 
     def test_no_shuffle(self):
-        dataset = ChunkedDataset(self.data_dir, shuffle=False)
-        items = []
-        for item in dataset:
-            items.append(item)
+        items = list(ChunkedDataset(self.data_dir, shuffle=False))
         self.assertListEqual(items, self.flattened_test_data)
 
 
     def test_shuffle(self):
-        dataset = ChunkedDataset(self.data_dir, shuffle=True)
-        items = []
-        for item in dataset:
-            items.append(item)
+        items = list(ChunkedDataset(self.data_dir, shuffle=True))
         self.assertSetEqual(set(items), set(self.flattened_test_data))
 
     
     def test_other_files_present(self):
         with open(os.path.join(self.data_dir, 'i_do_not_belong_here.txt'), 'w') as f:
             f.write('really ...')
-
-        dataset = ChunkedDataset(self.data_dir, shuffle=False)
-        items = []
-        for item in dataset:
-            items.append(item)
+        items = list(ChunkedDataset(self.data_dir, shuffle=False))
         self.assertListEqual(items, self.flattened_test_data)
 
 
     def test_transform(self):
         transform = lambda s: s + '!'
         modified_test_data = [transform(s) for s in self.flattened_test_data]
-        dataset = ChunkedDataset(self.data_dir, shuffle=False, transform=transform)
-        items = []
-        for item in dataset:
-            items.append(item)
+        items = list(ChunkedDataset(self.data_dir, shuffle=False, transform=transform))
         self.assertListEqual(items, modified_test_data)
 
 
