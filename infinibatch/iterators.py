9cf60ab4e5d80113634c99d05ae2d4fe4590f541
Frank Seide <fseide@microsoft.com>
2020-02-11 15:32:33 -0800

    changed ChunkedDatasetIterator to a convenience function

--- infinibatch/iterators.py
+++ infinibatch/iterators.py
@@ -282,62 +282,50 @@ class TransformIterator(CheckpointableIterator):
 
     def __next__(self):
         return self._transform(next(self._input_iterator))
-        
 
-# @TODO: This is no more than a factory function. We should change it to one
-class ChunkedDatasetIterator(CheckpointableIterator):
-    def __init__(self, paths: Union[str, Iterable[str]], shuffle: bool=True, buffer_size: int=2**20, transform: Callable[[Any],Any]=None, seed: Optional[int]=None, num_instances: int=1, instance_rank: int=0):
-        """
-        Dataset reading data from gzipped chunks.
 
-        This dataset infinitely repeats the data.
+def ChunkedDatasetIterator(paths: Union[str, Iterable[str]], shuffle: bool=True, buffer_size: int=2**20, transform: Callable[[Any],Any]=None, seed: Optional[int]=None, num_instances: int=1, instance_rank: int=0):
+    """
+    Dataset reading data from gzipped chunks.
 
-        Args:
-            paths: path, or list of paths, of directory containing dataset, i.e., a collection of .gz-files containing compressed text
-            shuffle: if true, the data is shuffled
-            buffer_size: size of the buffer in number of samples / data items used for shuffling
-            transform: transform to be applied to each data item (transform(Any) -> Any)
-            seed: random seed (or None)
-            num_instances: number of instances of this dataset. Meant for use with multi-process data loading, e.g., in distributed training.
-            instance_rank: rank of this instance of the dataset. Meant for use with multi-process data loading, e.g., in distributed training.
-        """
-        if isinstance(paths, str):  # handle single string
-            paths = [paths]
-        # set up the chunk reader
-        chunk_file_paths = [  # enumerate all .gz files in the given paths
-            os.path.join(path, subpath.name)
-            for path in paths
-            for subpath in os.scandir(path)
-            if subpath.is_file() and subpath.name.endswith('.gz')
-        ]
-        chunk_file_paths.sort()  # make sure file order is always the same, independent of OS
-        chunks  = InfinitePermutationIterator(chunk_file_paths, seed, shuffle=shuffle, num_instances=num_instances, instance_rank=instance_rank)
-        # set up the item reader
-        samples = ChunkedDataIterator(chunks)
-        # set up the item randomizer
-        if shuffle:
-            # use different seed for BufferedShuffleGenerator
-            buffered_shuffle_iterator_seed = seed
-            if buffered_shuffle_iterator_seed is not None:
-                buffered_shuffle_iterator_seed += 1
-            samples = BufferedShuffleIterator(samples, buffer_size, buffered_shuffle_iterator_seed)
-        
-        # apply transform, if given
-        if transform is not None:
-            samples = TransformIterator(samples, transform)
+    This dataset infinitely repeats the data.
 
-        # this is what we are serving out
-        self._iterator = samples
-        self.setstate(None)
+    Args:
+        paths: path, or list of paths, of directory containing dataset, i.e., a collection of .gz-files containing compressed text
+        shuffle: if true, the data is shuffled
+        buffer_size: size of the buffer in number of samples / data items used for shuffling
+        transform: transform to be applied to each data item (transform(Any) -> Any)
+        seed: random seed (or None)
+        num_instances: number of instances of this dataset. Meant for use with multi-process data loading, e.g., in distributed training.
+        instance_rank: rank of this instance of the dataset. Meant for use with multi-process data loading, e.g., in distributed training.
+    """
+    if isinstance(paths, str):  # handle single string
+        paths = [paths]
+    # set up the chunk reader
+    chunk_file_paths = [  # enumerate all .gz files in the given paths
+        os.path.join(path, subpath.name)
+        for path in paths
+        for subpath in os.scandir(path)
+        if subpath.is_file() and subpath.name.endswith('.gz')
+    ]
+    chunk_file_paths.sort()  # make sure file order is always the same, independent of OS
+    chunks  = InfinitePermutationIterator(chunk_file_paths, seed, shuffle=shuffle, num_instances=num_instances, instance_rank=instance_rank)
+    # set up the item reader
+    samples = ChunkedDataIterator(chunks)
+    # set up the item randomizer
+    if shuffle:
+        # use different seed for BufferedShuffleGenerator
+        buffered_shuffle_iterator_seed = seed
+        if buffered_shuffle_iterator_seed is not None:
+            buffered_shuffle_iterator_seed += 1
+        samples = BufferedShuffleIterator(samples, buffer_size, buffered_shuffle_iterator_seed)
+    
+    # apply transform, if given
+    if transform is not None:
+        samples = TransformIterator(samples, transform)
 
-    def getstate(self):
-        return self._iterator.getstate()
-
-    def setstate(self, checkpoint):
-        self._iterator.setstate(checkpoint)
-
-    def __next__(self):
-        return next(self._iterator)
+    # this is what we are serving out
+    return samples
 
 
 # However, that may no longer be true with checkpointing, so let's keep it as a class for now.
