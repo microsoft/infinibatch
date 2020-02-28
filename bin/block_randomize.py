d37cce9e46f6ad003f3df683ac6588334035f6a5
Frank Seide <fseide@microsoft.com>
2020-02-28 15:54:48 -0800

    chunked_dataset_iterator now accepts a list of chunk refs instead of a directory path

--- bin/block_randomize.py
+++ bin/block_randomize.py
@@ -13,15 +13,25 @@ from infinibatch.files_and_blobs import find_files, read_utf8_file
 
 if sys.argv[1] == "--azure-storage-key":
     credential = sys.argv[2]
-    sets = sys.argv[3:]
+    paths = sys.argv[3:]
 else:
     credential = None
-    sets = sys.argv[1:]
+    paths = sys.argv[1:]
 
-def readlines_from_zipped(textfile_path: str) -> Iterable[str]:
+def readlines_from_zipped(textfile_path: str):
     #print("chunked_dataset_iterator: reading", textfile_path, file=sys.stderr)
     return iter(read_utf8_file(textfile_path, credential).splitlines())
 
-ds = chunked_dataset_iterator(sets, read_chunk_fn=readlines_from_zipped, shuffle=True, buffer_size=1000000, seed=1, credentials=credential)
+if isinstance(paths, str):  # handle single string
+    paths = [paths]
+chunk_file_paths = [  # enumerate all .gz files in the given paths
+    subpath
+    for path in paths
+    for subpath in find_files(path, '.gz', credential)
+]
+chunk_file_paths.sort()  # make sure file order is always the same, independent of OS
+#print("chunked_dataset_iterator: reading from", len(chunk_file_paths), "chunk files", file=sys.stderr)
+
+ds = chunked_dataset_iterator(chunk_file_paths, read_chunk_fn=readlines_from_zipped, shuffle=True, buffer_size=1000000, seed=1, credentials=credential)
 for line in ds:
     print(line)
--- infinibatch/datasets.py
+++ infinibatch/datasets.py
@@ -15,7 +15,7 @@ def bump_seed(seed: Optional[int], step = 1):
     return None if seed is None else seed + 1
 
 
-def chunked_dataset_iterator(paths: Union[str, Iterable[str]], read_chunk_fn: Callable[[Any], Iterator],
+def chunked_dataset_iterator(chunk_refs: Iterable, read_chunk_fn: Callable[[Any], Iterator],
                              shuffle: bool=True, buffer_size: int=2**20,
                              transform: Callable[[Any],Any]=None,
                              prefetch_buffer_size: int=2**20,
@@ -28,7 +28,7 @@ def chunked_dataset_iterator(paths: Union[str, Iterable[str]], read_chunk_fn: Ca
     This dataset infinitely repeats the data.
 
     Args:
-        paths: path, or list of paths, of directory containing dataset, i.e., a collection of .gz-files containing compressed text
+        chunk_refs: references (such as path names) to chunk files
         read_chunk_fn: function(chunk_ref) to read a chunk's content into an iterator over its items
         shuffle: if true, the data is shuffled
         buffer_size: size of the buffer in number of samples / data items used for shuffling (default: 2**20)
@@ -40,17 +40,8 @@ def chunked_dataset_iterator(paths: Union[str, Iterable[str]], read_chunk_fn: Ca
         credentials: Azure container credentials, either a string or a dict [account] -> key (or None)
         use_block: temporary option to switch to BlockShuffleIterator (used for comparing against WindowsShuffleIterator). Will go away.
     """
-    if isinstance(paths, str):  # handle single string
-        paths = [paths]
     # set up the chunk reader
-    chunk_file_paths = [  # enumerate all .gz files in the given paths
-        subpath
-        for path in paths
-        for subpath in find_files(path, '.gz', credentials)
-    ]
-    chunk_file_paths.sort()  # make sure file order is always the same, independent of OS
-    #print("chunked_dataset_iterator: reading from", len(chunk_file_paths), "chunk files", file=sys.stderr)
-    chunks  = InfinitePermutationIterator(chunk_file_paths, seed, shuffle=shuffle, num_instances=num_instances, instance_rank=instance_rank)
+    chunks  = InfinitePermutationIterator(chunk_refs, seed, shuffle=shuffle, num_instances=num_instances, instance_rank=instance_rank)
     # set up the item reader
     samples = SelectManyIterator(source_iterator=chunks, collection_selector=read_chunk_fn)
     # wrap the I/O operation in a prefetch iterator
